cae8007a07aa539981191df9867077c504e57af0
Mark Tanner <mark.tanner@pagesuite.com>
2020-01-02 09:11:13 +0000

    added zoom out in menu

--- lib/src/view/MainMenu.dart
+++ lib/src/view/MainMenu.dart
@@ -13,6 +13,11 @@ class MainMenu extends stagexl_main_menu.MainMenu {
     stagexl_main_menu.MenuItem view = addMenuItem("View")
       ..onMouseClick.listen(_refreshViewMenu, priority: 1);
 
+    view.addMenuItem("Zoom Out")
+      ..onMouseClick.listen((_) {
+        MainWindow.resetCanvasZoomAndPosition();
+      });
+
     _showPropertyWindow = view.addCheckboxMenuItem("Show Property Window", true)
       ..onIsCheckedChanged.listen((_) {
         MainWindow.propertyWindow.visible = _showPropertyWindow.isChecked;
